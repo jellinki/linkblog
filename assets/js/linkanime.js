36b9f4790db481aae48e30f63b327ff412129892
jellinki <katelynjelli@gmail.com>
2023-09-27 14:39:49 -0700

    attempted to make an animation in js

---
+++ assets/js/linkanime.js
@@ -0,0 +1,25 @@
+// Get a reference to the image element
+const animatedImage = new Image();
+animatedImage.src = "C:/Users/katel/OneDrive/kvscode/new blog/linkblog/images/clearheartlink.png";
+document.body.appendChild(animatedImage);
+
+// Set initial position and variables
+let positionX = 0;
+const speed = 2;
+
+// Function to update the image's position
+function animateImage() {
+    positionX += speed;
+    animatedImage.style.position = "absolute";
+    animatedImage.style.left = positionX + "px";
+
+    // Reset the position when the image goes off-screen
+    if (positionX < window.innerWidth) {
+        requestAnimationFrame(animateImage);
+    }
+}
+
+// Start the animation when the page loads
+window.addEventListener("load", () => {
+    animateImage();
+});

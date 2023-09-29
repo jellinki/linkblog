73f16f7926864110d0693a3c021e9ba9413a6a4e
jellinki <katelynjelli@gmail.com>
2023-09-28 20:31:31 -0700

    updated .js file

--- assets/js/link.js
+++ assets/js/link.js
@@ -1,31 +1,31 @@
 const canvas = document.getElementById('canvasl');
 const ctx = canvas.getContext('2d');
-console.log(ctx);
 
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
 const playerImage = new Image();
+playerImage.onload = animate; // Call animate() once the image is loaded
 playerImage.src = 'images/simplelink.png';
+
 const spriteWidth = 21;
 const spriteHeight = 30;
-let frameX =  0;
+let frameX = 0;
 let frameY = 0;
 let gameFrame = 0;
 const staggerFrames = 5;
 
-function animate(){
+function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    //ctx.fillRect(100,50,100,100);
-    //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh)
-    ctx.drawImage(playerImage,  frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-    requestAnimationFrame(animate);
-    if (gameFrame % staggerFrames == 0){
+    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    
+    if (gameFrame % staggerFrames == 0) {
         if (frameX < 4) frameX++;
         else frameX = 0;
     }
 
     gameFrame++;
     requestAnimationFrame(animate);
-};
-animate();
+}
+
+// Don't call requestAnimationFrame here; it's already called inside the animate function.
